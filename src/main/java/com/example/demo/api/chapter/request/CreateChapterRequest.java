00745c0a75ff7bea6b83163d4a2511426ca5b617
Hieu <danghieu14th@gmail.com>
2023-03-31 23:53:50 +0700

    test

--- src/main/java/com/example/demo/api/chapter/request/CreateChapterRequest.java
+++ src/main/java/com/example/demo/api/chapter/request/CreateChapterRequest.java
@@ -4,9 +4,12 @@ package com.example.demo.api.chapter.request;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class CreateChapterRequest {
+    @NotNull
     private Long bookId;
     private String title;
     private String content;
